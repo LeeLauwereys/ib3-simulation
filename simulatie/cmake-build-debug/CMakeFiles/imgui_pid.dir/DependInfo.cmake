12a996fe0df9432d5e9ef0d75b0fa9d6d016cb0e
LeeLauwereys <102731812+LeeLauwereys@users.noreply.github.com>
2026-05-18 18:50:48 +0200

    New simulation version

---
+++ simulatie/cmake-build-debug/CMakeFiles/imgui_pid.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/resource.rc" "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/cmake-build-debug/CMakeFiles/imgui_pid.dir/resource.rc.obj"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/."
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/imgui"
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/imgui/backends"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/imgui/backends/imgui_impl_dx11.cpp" "CMakeFiles/imgui_pid.dir/imgui/backends/imgui_impl_dx11.cpp.obj" "gcc" "CMakeFiles/imgui_pid.dir/imgui/backends/imgui_impl_dx11.cpp.obj.d"
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/imgui/backends/imgui_impl_win32.cpp" "CMakeFiles/imgui_pid.dir/imgui/backends/imgui_impl_win32.cpp.obj" "gcc" "CMakeFiles/imgui_pid.dir/imgui/backends/imgui_impl_win32.cpp.obj.d"
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/imgui/imgui.cpp" "CMakeFiles/imgui_pid.dir/imgui/imgui.cpp.obj" "gcc" "CMakeFiles/imgui_pid.dir/imgui/imgui.cpp.obj.d"
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/imgui/imgui_draw.cpp" "CMakeFiles/imgui_pid.dir/imgui/imgui_draw.cpp.obj" "gcc" "CMakeFiles/imgui_pid.dir/imgui/imgui_draw.cpp.obj.d"
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/imgui/imgui_tables.cpp" "CMakeFiles/imgui_pid.dir/imgui/imgui_tables.cpp.obj" "gcc" "CMakeFiles/imgui_pid.dir/imgui/imgui_tables.cpp.obj.d"
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/imgui/imgui_widgets.cpp" "CMakeFiles/imgui_pid.dir/imgui/imgui_widgets.cpp.obj" "gcc" "CMakeFiles/imgui_pid.dir/imgui/imgui_widgets.cpp.obj.d"
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/main.cpp" "CMakeFiles/imgui_pid.dir/main.cpp.obj" "gcc" "CMakeFiles/imgui_pid.dir/main.cpp.obj.d"
+  "C:/Users/lauwe/Self-driving-bicycle-2025-2026/src/balance/simulatie/pid.cpp" "CMakeFiles/imgui_pid.dir/pid.cpp.obj" "gcc" "CMakeFiles/imgui_pid.dir/pid.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ simulatie/cmake-build-debug/CMakeFiles/imgui_pid.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/imgui_pid.dir/imgui/backends/imgui_impl_dx11.cpp.obj"
+  "CMakeFiles/imgui_pid.dir/imgui/backends/imgui_impl_dx11.cpp.obj.d"
+  "CMakeFiles/imgui_pid.dir/imgui/backends/imgui_impl_win32.cpp.obj"
+  "CMakeFiles/imgui_pid.dir/imgui/backends/imgui_impl_win32.cpp.obj.d"
+  "CMakeFiles/imgui_pid.dir/imgui/imgui.cpp.obj"
+  "CMakeFiles/imgui_pid.dir/imgui/imgui.cpp.obj.d"
+  "CMakeFiles/imgui_pid.dir/imgui/imgui_draw.cpp.obj"
+  "CMakeFiles/imgui_pid.dir/imgui/imgui_draw.cpp.obj.d"
+  "CMakeFiles/imgui_pid.dir/imgui/imgui_tables.cpp.obj"
+  "CMakeFiles/imgui_pid.dir/imgui/imgui_tables.cpp.obj.d"
+  "CMakeFiles/imgui_pid.dir/imgui/imgui_widgets.cpp.obj"
+  "CMakeFiles/imgui_pid.dir/imgui/imgui_widgets.cpp.obj.d"
+  "CMakeFiles/imgui_pid.dir/main.cpp.obj"
+  "CMakeFiles/imgui_pid.dir/main.cpp.obj.d"
+  "CMakeFiles/imgui_pid.dir/pid.cpp.obj"
+  "CMakeFiles/imgui_pid.dir/pid.cpp.obj.d"
+  "CMakeFiles/imgui_pid.dir/resource.rc.obj"
+  "imgui_pid.exe"
+  "imgui_pid.exe.manifest"
+  "imgui_pid.pdb"
+  "libimgui_pid.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX RC)
+  include(CMakeFiles/imgui_pid.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
